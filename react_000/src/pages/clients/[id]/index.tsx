7b1ded419a777f45f746f44ef1fe29aaf8e0831f
CodeBreaker <32624090+Xiao009@users.noreply.github.com>
2023-11-29 23:16:42 -0500

    final update here

--- react_000/src/pages/clients/[id]/index.tsx
+++ react_000/src/pages/clients/[id]/index.tsx
@@ -4,12 +4,16 @@ export default function ClientProjectPage() {
   const router = useRouter();
   console.log(router.query);
   function loadProjectHandler() {
-    router.push(`/clients/max/projecta`);
+    // router.push(`/clients/max/projecta`);
+    router.push({
+      pathname: `/clients/[id]/[clientprojectid]`,
+      query: { id: `max`, clientprojectid: "projecta" },
+    });
   }
   return (
     <div>
       <h1>The projects of a given client</h1>
-      <button>Load Project A</button>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
