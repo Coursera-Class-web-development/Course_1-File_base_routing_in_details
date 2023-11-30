1da59f9ee648cf77cc353372ceb1de5b8e3f3038
CodeBreaker <32624090+Xiao009@users.noreply.github.com>
2023-11-29 22:14:03 -0500

    update

--- react_000/src/pages/index.tsx
+++ react_000/src/pages/index.tsx
@@ -1,7 +1,14 @@
-export default function HomePage(){
-    return (
-      <div>
-        <h1>The page</h1>
-      </div>
-    );
+import Link from "next/link";
+export default function HomePage() {
+  return (
+    <div>
+      <h1>The Home page</h1>
+      <ul>
+        <li>
+          {/* <a href="/portfolio">Portfolio</a> */}
+          <Link href="/portfolio">Portfolio</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
