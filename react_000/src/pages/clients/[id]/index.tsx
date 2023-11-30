19f29ea055d662b6d78216dc99df0ed7337da8dc
CodeBreaker <32624090+Xiao009@users.noreply.github.com>
2023-11-29 23:07:19 -0500

    section 1 four classes left
    11/29

--- react_000/src/pages/clients/[id]/index.tsx
+++ react_000/src/pages/clients/[id]/index.tsx
@@ -1,7 +1,15 @@
-export default function ClientProjectPage(){
-    return (
-      <div>
-        <h1>The Client Project page</h1>
-      </div>
-    );
+import { useRouter } from "next/router";
+
+export default function ClientProjectPage() {
+  const router = useRouter();
+  console.log(router.query);
+  function loadProjectHandler() {
+    router.push(`/clients/max/projecta`);
+  }
+  return (
+    <div>
+      <h1>The projects of a given client</h1>
+      <button>Load Project A</button>
+    </div>
+  );
 }
--- react_000/src/pages/clients/index.tsx
+++ react_000/src/pages/clients/index.tsx
@@ -1,7 +1,25 @@
-export default function ClientPage(){
-    return (
-      <div>
-        <h1>The Client page</h1>
-      </div>
-    );
+import Link from "next/link";
+
+export default function ClientPage() {
+  const clients = [
+    { id: "max", name: "Maximilian" },
+    { id: "manu", name: "Manuel" },
+  ];
+  return (
+    <div>
+      <h1>The Client page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            {/* <Link href={`/clients/${client.id}`}>{client.name}</Link> */}
+            <Link
+              href={{ pathname: "/clients/[id]", query: { id: client.id } }}
+            >
+              {client.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
